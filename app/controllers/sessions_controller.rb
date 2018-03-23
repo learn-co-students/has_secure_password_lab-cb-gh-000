84b1ab6176e77098cf26e5bfaa2bd37499f3d81d
platrol <takialog@gmail.com>
2018-03-23 15:54:05 +0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
+    binding.pry
     user = User.find_by(name: params[:user][:name])
     return head(:forbidden) unless user.authenticate(params[:user][:password])
     session[:user_id] = user.id
